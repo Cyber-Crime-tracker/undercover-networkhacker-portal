6c36186d1221602b287b6dfdc96911f7994c4a4c
Cyber-Crime-tracker <76731045+Cyber-Crime-tracker@users.noreply.github.com>
2021-01-22 22:48:55 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -8,48 +8,48 @@ function hh(){
 			
 			else if(a=="F7C7F591"){
 				
-				document.getElementById("demo1").innerHTML="<audio controls><source src=index2.mp3 type=audio/mp3></audio>";
+				document.getElementById("demo1").innerHTML="<audio controls><source src=index2.mp3 type=audio/mp3></audio><div><p style=color:white>Call tracked by K.AFREEN (Network Hacker)</p></div>";
 				
 				
 			}
 			
 			else if(a=="611916D7"){
 			
-				document.getElementById("demo1").innerHTML="<audio controls><source src=call/priya1.mp3 type=audio/mp3></audio>";
+				document.getElementById("demo1").innerHTML="<audio controls><source src=call/priya1.mp3 type=audio/mp3></audio><div><p style=color:white>Call tracked by K.AFREEN (Network Hacker)</p></div>";
 			
 			}
 			
 			else if(a=="2156ACEA"){
 			
-				document.getElementById("demo1").innerHTML="<audio controls><source src=call/priya2.mp3 type=audio/mp3></audio>";
+				document.getElementById("demo1").innerHTML="<audio controls><source src=call/priya2.mp3 type=audio/mp3></audio><div><p style=color:white>Call tracked by K.AFREEN (Network Hacker)</p></div>";
 			
 			
 			}
 			
 			else if(a=="5D8FFCC8"){
 			
-				document.getElementById("demo1").innerHTML="<audio controls><source src=call/priya3.mp3 type=audio/mp3></audio>";
+				document.getElementById("demo1").innerHTML="<audio controls><source src=call/priya3.mp3 type=audio/mp3></audio><div><p style=color:white>Call tracked by K.AFREEN (Network Hacker)</p></div>";
 			}
 			
 			else if(a=="D21971F7"){
 			
-				document.getElementById("demo1").innerHTML="<audio controls><source src=call/priya4.mp3 type=audio/mp3></audio>";
+				document.getElementById("demo1").innerHTML="<audio controls><source src=call/priya4.mp3 type=audio/mp3></audio><div><p style=color:white>Call tracked by K.AFREEN (Network Hacker)</p></div>";
 			
 			}
 			
 			else if(a=="O2A6CC3A"){
 			
-				document.getElementById("demo1").innerHTML="<audio controls><source src=call/priya5.mp3 type=audio/mp3></audio>";
+				document.getElementById("demo1").innerHTML="<audio controls><source src=call/priya5.mp3 type=audio/mp3></audio><div><p style=color:white>Call tracked by K.AFREEN (Network Hacker)</p></div>";
 			}
 				
 			else if(a=="3DBF1472"){
 			
-				document.getElementById("demo1").innerHTML="<audio controls><source src=call/priya6.mp3 type=audio/mp3></audio>";
+				document.getElementById("demo1").innerHTML="<audio controls><source src=call/priya6.mp3 type=audio/mp3></audio><div><p style=color:white>Call tracked by K.AFREEN (Network Hacker)</p></div>";
 			}
 				
 			else if(a=="CE667DA1"){
 			
-				document.getElementById("demo1").innerHTML="<audio controls><source src=call/demo.mp3 type=audio/mp3></audio>";
+				document.getElementById("demo1").innerHTML="<audio controls><source src=call/demo.mp3 type=audio/mp3></audio><div><p style=color:white>Call tracked by K.AFREEN (Network Hacker)</p></div>";
 			}
 	
 				
